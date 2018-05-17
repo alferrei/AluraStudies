8ddb0fe9e92805f9f7f27a1adf79568dde67ca9e
Alexandre.Ferreira <Alexandre.Ferreira@LAPTOP-IGOD4CQA.provider-it.local>
2018-05-17 17:07:57 -0300

    Começo Aula 3

--- Alura3/src/FuncionarioTeste.java
+++ Alura3/src/FuncionarioTeste.java
@@ -1,12 +1,28 @@
 
-public class Funcionario {
+public class FuncionarioTeste {
+
 	private String nome;
 	private String cpf;
 	private double salario;
+	private int tipo =0;
 	
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
 	public double getBonificacao() {
-		return this.salario*0.1;
 		
+		if (this.tipo==0) {
+			return this.salario*0.1;
+		} else if (this.tipo==1){
+			return this.salario;
+		}else {
+			return this.salario+1000.0;
+		}
 	}
 	
 	public String getNome() {
@@ -31,3 +47,4 @@ public class Funcionario {
 	
 	
 }
+
